0bbe356024b1eaf6ac9c736ec3077eea8434cb6b
NishatFariza <nishatfarzia78@gmail.com>
2022-01-29 00:16:49 +0600

    for loop an array

---
+++ array-loop.js
@@ -0,0 +1,19 @@
+// var numbers=[12, 13, 16, 22, 43, 54, 77, 88];
+// for(var i=1; i<8; i++){
+//     var element =numbers[i];
+//     console.log(element);
+
+// }
+    
+// var numbers=[12, 13, 16, 22, 43, 54, 77, 88];
+// for(var i=0; i< numbers.lenght; i++){
+//     var element = numbers[i];
+//     console.log(element);
+
+// }   
+var items =['bottol', 'mobile', 'charger', 'watch', 'book'];
+for (var i=0; i<items.length; i++){
+    var item =items[i];
+    console.log(item);
+
+}

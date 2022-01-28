f6c220f84dee8cdf4717db8848ee727e81dedb8a
NishatFariza <nishatfarzia78@gmail.com>
2022-01-28 23:16:25 +0600

    for loop vs while loop practice

---
+++ for-loop.js
@@ -0,0 +1,29 @@
+// for loop 
+// for (var roastGiven=1; roastGiven<7; roastGiven++){
+//     console.log('Roast den, Please!');
+
+// }
+// for loop 
+// for (var number=1; number<10; number++){
+//     console.log('Running a for loop');
+//     console.log(number);
+
+// }
+// for loop 
+// for (var roastGiven=1; roastGiven<7; roastGiven++){
+//     console.log('Roast den, Please!');
+
+// for(var i=0; i<=50; i=i+2){
+//     console.log(i);
+
+// }
+// odd number 
+// for(var i=1; i<=50; i+=2){
+//     console.log(i);
+
+// }
+// even number 
+for(var i=0; i< 50; i+=2){
+    console.log(i);
+
+}

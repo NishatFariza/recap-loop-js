7ec8e02ee9a4251683ef43786a4b6b600d8b8bea
NishatFariza <nishatfarzia78@gmail.com>
2022-01-28 23:31:18 +0600

    for loop and while loop summary practice

---
+++ summary.js
@@ -0,0 +1,12 @@
+// basic while loop 
+var number=1;
+while (number<=20){
+    console.log('This is number of while loop');
+    number++;
+
+}
+//basic of for loop
+for (var i=1; i<=30; i+=2){
+    console.log('this is for loop');
+
+}
